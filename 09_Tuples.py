a0d3c6a6278799e5505f576515e37a5c4f5ac0a5
saursaku <saursaku@SAURSAKU-IN.idc.oracle.com>
2019-08-14 21:16:18 +0530

    Completed tuple function

--- 09_Tuples.py
+++ 09_Tuples.py
@@ -1,4 +1,8 @@
 if __name__ == '__main__':
     n = int(input())
     integer_list = map(int, input().split())
+    tuple_list = list()
+    for value in integer_list:
+        tuple_list.append(value)
+    print(hash(tuple(tuple_list)))
 
